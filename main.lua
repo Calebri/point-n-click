db5b9fd041cbab9ec802634b18675c52ef7f02d7
Calebri <caleblseamans@gmail.com>
2026-04-30 10:16:18 -0400

    Added main rooms and CBs

--- main.lua
+++ main.lua
@@ -30,12 +30,38 @@ if debug then
     }
 else -- \/ MAIN SCENES LIST \/
     scenes = {}
+
     scenes[1] = Scene(Assets.GetImg("img/scenes/lobby.png"), -- Lobby
-                        {Cb(270, 158, 381, 264, {trans=2})})
-    scenes[2] = Scene(Assets.GetImg("img/scenes/checkin.png"), -- Check-in
-                        {Cb(250, 370, 400, 480, {trans=1})},
-                        {Addon(Assets.GetImg("img/scenes/checkin_key.png", true))})
-    scenes[3] = Scene(Assets.GetImg("img/scenes/hallway.png")) -- Hallway
+                        {Cb(270, 158, 381, 264, {trans=2}), -- Check-In Box
+                         Cb(160,  44, 489, 250), -- Area Behind
+                         Cb( 75, 239, 160, 331), -- Lamp 1
+                         Cb(489, 239, 573, 331), -- Lamp 2
+                        })
+    
+    scenes[2] = Scene(Assets.GetImg("img/scenes/checkin.png"), -- Check-In
+                        {Cb(250, 370, 400, 480, {trans=1}) -- Arrow Button
+                        },
+                        {Addon(Assets.GetImg("img/scenes/checkin_key.png", true))}) -- Key
+    
+    scenes[3] = Scene(Assets.GetImg("img/scenes/hallway.png"), -- Hallway
+                        {Cb( 79,  73, 160, 490), -- Door L1
+                         Cb(185, 128, 225, 401), -- Door L2
+                         Cb(240, 158, 256, 336), -- Door L3
+                         Cb(291, 168, 353, 300), -- Door Center
+                         Cb(387, 158, 403, 336), -- Door R1
+                         Cb(418, 128, 459, 401), -- Door R2
+                         Cb(485,  73, 565, 490), -- Door R3
+                        })
+    
+    scenes[4] = Scene(Assets.GetImg("img/scenes/room.png"), -- Hotel Room
+                        {Cb(  6, 410, 117, 473), -- Note
+                         Cb(122, 330, 443, 401), -- Bed
+                         Cb(101, 137, 231, 234), -- Painting
+                         Cb(513,  80, 427, 333), -- Wardrobe
+                         Cb(462,  77, 612, 277), -- Window
+                         Cb(401, 408, 542, 489), -- Suitcase
+                        })
+
 end
 
 local sceneman = SceneGroup(scenes, 1)
