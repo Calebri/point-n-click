5add8ce76795bc4e3da632c40e270f31bd6ab578
Calebri <caleblseamans@gmail.com>
2026-05-05 10:49:44 -0400

    Story update

--- main.lua
+++ main.lua
@@ -34,35 +34,40 @@ else -- \/ MAIN SCENES LIST \/
 
     scenes[1] = Scene(Assets.GetImg("img/scenes/lobby.png"), -- Lobby
                         {Cb(270, 158, 381, 264, {trans=2}), -- Check-In Box
-                         Cb( 40,   0, 611,  80, {trans=3}) -- Placeholder Top Doors CB
+                         Cb( 40,   0, 611,  80, {trans=3}), -- Placeholder Top Doors CB
+                         Cb(145, 125, 411, 153, {opentextbox="It says \"CHECK-IN\"."}), -- Check-In Text
+                         Cb(235, 264, 407, 415, {opentextbox="There are some stairs leading up to a small booth."}), -- Front stairs
+                         Cb(  0, 460, 650, 490, {opentextbox="No going back now."})
                         })
     
     scenes[2] = Scene(Assets.GetImg("img/scenes/checkin.png"), -- Check-In
                         {Cb(250, 370, 400, 480, {trans=1}), -- Arrow Button
-                         Cb(160,  44, 489, 250), -- Area Behind
-                         Cb( 75, 239, 160, 331), -- Lamp 1
-                         Cb(489, 239, 573, 331), -- Lamp 2
+                         Cb(160,  44, 489, 250, {opentextbox="There's nobody there."}), -- Area Behind
+                         Cb( 75, 239, 160, 331, {opentextbox="It's a small lamp."}), -- Lamp 1
+                         Cb(489, 239, 573, 331, {opentextbox="It's a small lamp."}), -- Lamp 2
                          Cb(343, 309, 392, 330, {setflags={keyCollected=true}, opentextbox="You picked up the key."}, {flags={"_keyCollected"}}), -- Key
                         },
                         {Addon(Assets.GetImg("img/scenes/checkin_key.png"), {flags={"_keyCollected"}})}) -- Key
     
     scenes[3] = Scene(Assets.GetImg("img/scenes/hallway.png"), -- Hallway
-                        {Cb( 79,  73, 160, 490), -- Door L1
-                         Cb(185, 128, 225, 401), -- Door L2
-                         Cb(240, 158, 256, 336), -- Door L3
-                         Cb(291, 168, 353, 300), -- Door Center
-                         Cb(387, 158, 403, 336), -- Door R1
-                         Cb(418, 128, 459, 401), -- Door R2
-                         Cb(485,  73, 565, 490), -- Door R3
-                        })
+                        {Cb( 79,  73, 160, 490, {opentextbox="It's locked"}), -- Door L1
+                         Cb(185, 128, 225, 401, {opentextbox="It's locked"}), -- Door L2
+                         Cb(240, 158, 256, 336, {opentextbox="It's locked"}), -- Door L3
+                         Cb(291, 168, 353, 300, {switch={_keyCollected={opentextbox="It's locked"}, keyCollected={opentextbox="You unlock the door with the key.", trans=4}}}), -- Door Center
+                         Cb(387, 158, 403, 336, {opentextbox="It's locked"}), -- Door R1
+                         Cb(418, 128, 459, 401, {opentextbox="It's locked"}), -- Door R2
+                         Cb(485,  73, 565, 490, {opentextbox="It's locked"}), -- Door R3
+                         Cb(280, 430, 360, 480, {trans=1}) -- Turnaround
+                        },
+                        {Addon(Assets.GetImg("img/scenes/turnaround.png"))})
     
     scenes[4] = Scene(Assets.GetImg("img/scenes/room.png"), -- Hotel Room
-                        {Cb(  6, 410, 117, 473), -- Note
-                         Cb(122, 330, 443, 401), -- Bed
-                         Cb(101, 137, 231, 234), -- Painting
-                         Cb(513,  80, 427, 333), -- Wardrobe
-                         Cb(462,  77, 612, 277), -- Window
-                         Cb(401, 408, 542, 489), -- Suitcase
+                        {Cb(  6, 410, 117, 473, {opentextbox="NOTE TEXT HERE"}), -- Note
+                         Cb(122, 330, 443, 401, {opentextbox="You feel tired, but the developer hasn't yet added the ability for you to go to sleep."}), -- Bed
+                         Cb(101, 137, 231, 234, {opentextbox="A jovial painting. For some reason it makes you feel nauseous."}), -- Painting
+                         Cb(250,  80, 427, 333, {opentextbox="A large wardrobe."}), -- Wardrobe
+                         Cb(462,  77, 612, 277, {opentextbox="It looks like a window, but theres nothing on the other side."}), -- Window
+                         Cb(401, 408, 542, 489, {opentextbox="A small suitcase. It's locked."}), -- Suitcase
                         })
 
 end
